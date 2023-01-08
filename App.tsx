3be3c19689b7819b47bb69470efd5a94032463b1
alexbovt <bovttt@gmail.com>
2023-01-09 00:05:46 +0100

    initialize store, add test slice

--- App.tsx
+++ App.tsx
@@ -1,9 +1,31 @@
-import { NativeBaseProvider, Box } from 'native-base';
+import { Box, Button, NativeBaseProvider, Text } from 'native-base';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { store, RootState } from './store';
+import { counterSlice } from './test';
+
+function Test() {
+  const counter = useSelector<RootState, number>(s => s.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Button onPress={() => dispatch(counterSlice.actions.decrement())}>
+        -
+      </Button>
+      <Text>{counter}</Text>
+      <Button onPress={() => dispatch(counterSlice.actions.increment())}>
+        +
+      </Button>
+    </Box>
+  );
+}
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
-    </NativeBaseProvider>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <Test />
+      </NativeBaseProvider>
+    </Provider>
   );
 }
---
+++ test.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: state => {
+      state.value += 1;
+    },
+    decrement: state => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
